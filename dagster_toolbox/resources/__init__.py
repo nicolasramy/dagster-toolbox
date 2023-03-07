b29111810ec4b3d84f17e8a73d7df73ddf537e4d
Nicolas Ramy <nicolas.ramy@darkelda.com>
2023-03-07 12:23:42 +0100

    Fix typo

--- dagster_toolbox/resources/__init__.py
+++ dagster_toolbox/resources/__init__.py
@@ -1,6 +1,6 @@
 from .csv_partitioned_io_manager import csv_partitioned_io_manager
 from .json_partitioned_io_manager import json_partitioned_io_manager
-from .postgres_partitionned_io_manager import postgres_partitioned_io_manager
+from .postgres_partitioned_io_manager import postgres_partitioned_io_manager
 from .postgres_io_manager import postgres_io_manager
 from .object_storage import (
     analytics_objects,
@@ -21,7 +21,7 @@ __all__ = [
     # IO Managers
     "csv_partitioned_io_manager",
     "json_partitioned_io_manager",
-    "postgres_partitionned_io_manager",
+    "postgres_partitioned_io_manager",
     "postgres_io_manager",
     # ObjectStorage
     "analytics_objects",
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dagster-toolbox",
-    version="0.0.0",
+    version="0.0.1",
     packages=find_packages(),
     author="Nicolas RAMY",
     author_email="nicolas.ramy@darkelda.com",
