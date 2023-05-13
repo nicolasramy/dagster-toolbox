a053fdae3131f91d2fe0f3de5856ae3c42722660
Nicolas Ramy <nicolas.ramy@darkelda.com>
2023-05-13 11:07:18 +0200

    Add functions for dataframe

--- dagster_toolbox/functions/dataframe.py
+++ dagster_toolbox/functions/dataframe.py
@@ -1,10 +1,80 @@
+from datetime import datetime
+
+
 def cast_dataframe(dataframe, columns):
     for column_name, column_type in columns:
         if column_type == "string":
-            dataframe[column_name] = dataframe[column_name].astype(str)
+            dataframe[column_name] = dataframe[column_name].astype(
+                "str", errors="ignore"
+            )
+        elif column_type == "int":
+            dataframe[column_name] = dataframe[column_name].astype(
+                "int", errors="ignore"
+            )
         elif column_type == "float":
-            dataframe[column_name] = dataframe[column_name].astype(float)
+            dataframe[column_name] = dataframe[column_name].astype(
+                "float", errors="ignore"
+            )
+        elif column_type == "bool":
+            dataframe[column_name] = dataframe[column_name].astype(
+                "bool", errors="ignore"
+            )
+        elif column_type == "datetime":
+            dataframe[column_name] = dataframe[column_name].astype(
+                "datetime", errors="ignore"
+            )
+        elif column_type == "timedelta":
+            dataframe[column_name] = dataframe[column_name].astype(
+                "timedelta", errors="ignore"
+            )
+        elif column_type == "category":
+            dataframe[column_name] = dataframe[column_name].astype(
+                "category", errors="ignore"
+            )
         else:
-            dataframe[column_name] = dataframe[column_name].astype(str)
+            dataframe[column_name] = dataframe[column_name].astype(
+                "str", errors="ignore"
+            )
 
     return dataframe
+
+
+def get_date(value):
+    try:
+        return datetime.strptime(value, "%d/%m/%Y")
+    except ValueError:
+        return value
+
+
+def get_float_to_string(value):
+    try:
+        return str(int(float(value)))
+    except ValueError:
+        return value
+
+
+def get_float_from_string(value):
+    try:
+        return float(value)
+    except ValueError:
+        return value
+
+
+def get_int_from_string(value):
+    try:
+        return int(value)
+    except ValueError:
+        return value
+
+
+def get_string_from_float(value):
+    try:
+        return str(int(float(value)))
+    except ValueError:
+        return value
+
+
+def get_human_input_removed(value):
+    if value == "...":
+        return ""
+    return value
