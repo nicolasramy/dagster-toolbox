13be20bced16ffba25954f6fb02e95fbfdff47d3
Nicolas Ramy <nicolas.ramy@darkelda.com>
2023-10-13 10:06:09 +0200

    Improve ObjectStorage usage

--- README.md
+++ README.md
@@ -11,3 +11,41 @@ A set of tools to ease Dagster usage
 ```shell
 pip install dagster-toolbox
 ```
+
+## Usage
+
+#### New object storage resource
+
+Declare resource
+
+```python
+from dagster_toolbox.resources import ObjectStorage
+
+
+class Analytics(ObjectStorage):
+    bucket = "analytics"
+    
+
+@resource
+def analytics_objects(init_context):
+    return Analytics()
+
+
+RESOURCE_DEFS = {
+    "analytics_objects": analytics_objects,
+}
+
+```
+
+Use resource
+
+```python
+@asset
+def export_files(context):
+    partition_key = context.asset_partition_key_for_output()
+    file_path = f"analytics/data/{partition_key}"
+    
+    file_resource = context.resource.analytics_objects.read(file_path)    
+    ...
+
+```
--- dagster_toolbox/resources/__init__.py
+++ dagster_toolbox/resources/__init__.py
@@ -1,40 +1,19 @@
+from .abstract import ObjectStorage
 from .csv_partitioned_io_manager import csv_partitioned_io_manager
 from .json_partitioned_io_manager import json_partitioned_io_manager
 from .postgres_partitioned_io_manager import postgres_partitioned_io_manager
 from .postgres_io_manager import postgres_io_manager
-from .object_storage import (
-    analytics_objects,
-    connecting_objects,
-    customers_objects,
-    datalake_objects,
-    datawarehouse_objects,
-    experiments_objects,
-    external_sources_objects,
-    legacy_objects,
-    logs_objects,
-    miscellaneous_objects,
-    test_objects,
-)
 from .vault import vault
 
 
 __all__ = [
+    # Abstract
+    "ObjectStorage",
     # IO Managers
     "csv_partitioned_io_manager",
     "json_partitioned_io_manager",
     "postgres_partitioned_io_manager",
     "postgres_io_manager",
-    # ObjectStorage
-    "analytics_objects",
-    "connecting_objects",
-    "customers_objects",
-    "datalake_objects",
-    "datawarehouse_objects",
-    "experiments_objects",
-    "external_sources_objects",
-    "legacy_objects",
-    "logs_objects",
-    "miscellaneous_objects",
-    "test_objects",
+    # Vault
     "vault",
 ]
