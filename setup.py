793f8a806adc0339bd57055e73e0528705abf06b
Nicolas Ramy <nicolas.ramy@darkelda.com>
2024-09-04 11:03:25 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dagster-toolbox",
-    version="0.0.22",
+    version="0.0.23",
     packages=find_packages(),
     author="Nicolas RAMY",
     author_email="nicolas.ramy@darkelda.com",
